4a66498ceb9f68c2b93e6285b74ac2ff5ed2f36b
Gian Luigi Romita <romitagl@gmail.com>
2020-04-26 20:22:15 +0100

    Updated App to match new schema

--- frontend/react-kgraph/src/App.tsx
+++ frontend/react-kgraph/src/App.tsx
@@ -3,27 +3,34 @@ import { useQuery, gql } from '@apollo/client';
 
 import './App.css';
 
-const Query = gql`
+const LIST_USERS_TOPICS = gql`
   {
-    queryTenant { name id }
+    queryUser { name rootTopics { name } }
   }
 `;
 
-interface Query {
-  queryTenant: [{
-    id: number;
-    name: string;
+interface QueryUserTopics {
+  queryUser: [{
+    name: string,
+    rootTopics: [{name : string}]
   }];
 }
 
 function App() {
-  const { loading, error, data } = useQuery<Query>(Query);
+  const { loading, error, data } = useQuery<QueryUserTopics>(LIST_USERS_TOPICS);
   console.log(loading, error, data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
-    <div className="App">
-      {data !== undefined ? data.queryTenant.map((x: any) => <h1 key={x.id}>{x.name}</h1>) : null}
-    </div>
-  );
-}
+  <div className="App">
+    <header className="header">
+      <h1> Knowledge Graph </h1>
+        {data !== undefined ? data.queryUser.map((x: any) => <p>User:{x.name} {x.rootTopics.map((topic: any) => <div>{topic.name}</div>)}</p>) : <p>Error :(</p>}
+    </header>
+  </div>
+);
+  }
 
 export default App;

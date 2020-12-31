850cd0faecd44187714c61b3904cd297883de148
Gian Luigi Romita <romitagl@gmail.com>
2020-12-31 13:53:37 +0000

    Added GitHub Super-Linter

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # KGraph
 
 Knowledge Graph Project
+
+## Developer Notes
+
+All the pull requests to `master` have to pass the CI check [Super-Linter](https://github.com/github/super-linter).
+
+### Super Linter
+
+To setup locally the Super Linter follow [these instructions](https://github.com/github/super-linter/blob/main/docs/run-linter-locally.md).
+
+Run check on the local folder:
+
+```bash
+docker run -e RUN_LOCAL=true -e FILTER_REGEX_EXCLUDE=".*backend/hasura/schema/.*" -v `pwd`:/tmp/lint github/super-linter:v3.14.3
+```
--- backend/hasura/test/test_add_topic.sh
+++ backend/hasura/test/test_add_topic.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [[ ! "$HASURA_CURL_POST_COMMAND" || ! "$HASURA_GRAPHQL_ENDPOINT" ]]; then printf "missing required test env variables\n" && exit 1; fi
+if [[ ! $HASURA_CURL_POST_COMMAND || ! "$HASURA_GRAPHQL_ENDPOINT" ]]; then printf "missing required test env variables\n" && exit 1; fi
 
 USERNAME='ci_test'
 # 0. cleanup the users table
-$HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { delete_kgraph_users(where: {username: {_eq: \\\"$USERNAME\\\"}}) { affected_rows }}\" }" $HASURA_GRAPHQL_ENDPOINT
+$HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { delete_kgraph_users(where: {username: {_eq: \\\"$USERNAME\\\"}}) { affected_rows }}\" }" "$HASURA_GRAPHQL_ENDPOINT"
 
 # 1. create an entry in the users table
-INSERT_RET_VAL=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_users(objects: {username: \\\"$USERNAME\\\", display_name: \\\"$USERNAME\\\"}) { returning { created_at } } }\" }" $HASURA_GRAPHQL_ENDPOINT )
+INSERT_RET_VAL=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_users(objects: {username: \\\"$USERNAME\\\", display_name: \\\"$USERNAME\\\"}) { returning { created_at } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
 printf "created user: %s at: %s\n" "$USERNAME" "$INSERT_RET_VAL"
 
 # 2. add a topic
 TOPICNAME='test topic'
-TOPIC_ID_RESULT=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_topics(objects: {users_username: \\\"$USERNAME\\\", name: \\\"$TOPICNAME\\\"}) { returning { id } } }\" }" $HASURA_GRAPHQL_ENDPOINT )
+TOPIC_ID_RESULT=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_topics(objects: {users_username: \\\"$USERNAME\\\", name: \\\"$TOPICNAME\\\"}) { returning { id } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
 printf "added topic: %s with id: %s\n" "$TOPICNAME" "$TOPIC_ID_RESULT"
 
 # 3. get the topic by ID
 TOPIC_ID=$( echo "$TOPIC_ID_RESULT" | jq .data.insert_kgraph_topics.returning[0].id | tr -d '"' )
-TOPICNAME_RESULT=$( $HASURA_CURL_POST_COMMAND "{\"query\": \"query { kgraph_topics(where: {id: {_eq: \\\"$TOPIC_ID\\\" }}) { name }}\" }" $HASURA_GRAPHQL_ENDPOINT )
+TOPICNAME_RESULT=$( $HASURA_CURL_POST_COMMAND "{\"query\": \"query { kgraph_topics(where: {id: {_eq: \\\"$TOPIC_ID\\\" }}) { name }}\" }" "$HASURA_GRAPHQL_ENDPOINT" )
 printf "get topic: %s with id: %s\n" "$TOPICNAME_RESULT" "$TOPIC_ID"
 
 TOPICNAME_NAME=$( echo "$TOPICNAME_RESULT" | jq .data.kgraph_topics[0].name | tr -d '"' )

4450198b1bd3232984128ebfc70b4662a6e52b0b
Gian Luigi Romita <romitagl@gmail.com>
2021-01-16 19:07:22 +0000

    Added Visjs Graph Library + CI & Make fixes

--- frontend/vue/src/components/KGraphUser.vue
+++ frontend/vue/src/components/KGraphUser.vue
@@ -72,14 +72,21 @@
               <div v-else-if="data != null" class="result apollo">
                 {{ prevTopicName = topicName }}
                 <!-- {{ data }} -->
-                {{ getTopics(data) }}
+                {{ getTopics(data) }} {{ buildVisGraph() }}
               </div>
-
               <!-- No result -->
               <div v-else class="no-result apollo">No result :(</div>
             </template>
         </ApolloQuery>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <div id="vis-topics-graph"></div> 
+      </li>
+      <li>
         <div id="vuetreeselect">
+          Topics list:
           <!-- https://github.com/rangowuchen/ElementUIExample/blob/696672475cf35e2eee29cbdca518226c37e371b8/src/pages/vue-treeselect/components/moreFunction.vue -->
           <treeselect
             :multiple="true"
@@ -98,7 +105,13 @@
 import Treeselect from "@riophae/vue-treeselect";
 import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 
-function buildTopicsTree(topicsTree, kgraph_topics) {
+// https://github.com/visjs/vis-network
+import { Network } from "vis-network/peer/esm/vis-network";
+import { DataSet } from "vis-data/peer/esm/vis-data"
+
+// to consider: https://doc.mindelixir.ink/index.html - https://codesandbox.io/s/hygz7?file=/src/App.vue
+
+function buildTopicsTree(topicsTree, topicsRelations, kgraph_topics) {
   // console.log(kgraph_topics)
   if(Array.isArray(kgraph_topics)) {
     for (var counter = 0; counter < kgraph_topics.length; counter++) {
@@ -107,6 +120,15 @@ function buildTopicsTree(topicsTree, kgraph_topics) {
       const element = {
         id: topic.id,
         label: topic.name,
+      }
+      if (topic.parent_id != null) {
+        // console.log("topic id:" + topic.id + " parent: " + topic.parent_id)
+        const relation = {
+          id: topic.id,
+          from: topic.id,
+          to: topic.parent_id
+        }
+        topicsRelations.push(relation);
       }
       topicsTree.push(element);
     }
@@ -119,18 +141,35 @@ export default {
       username: '',
       selectedUser: '',
       topicName: '',
-      prevTopicName: ' ',
+      prevTopicName: ' ', // developer note -> initialized as space or not empty string
       topicAddedResult: '',
       // vue-treeselect
       topics: [],
+      topicsRelations: [],
+      // vis-network
+      network: null,
+      // nodes: new DataSet([{ id: "1", label: "Example topic 1" }, { id: "2", label: "Example topic 2" }]),
+      nodes: new DataSet([]),
+      // edges: new DataSet([{ id: "1", from: "1", to: "2" }]),
+      edges: new DataSet([]),
+      displayOptions: { }
     }
   },
-
+  created() {
+    this.network = null;
+  },
+  mounted() {
+    this.buildVisGraph()
+  },
+  beforeDestroy() {
+    if (this.network) {
+      this.network.destroy();
+    }
+  },
   computed: {
   },
   // register the component
   components: { Treeselect },
-
   methods: {
     onTopicAdded: function (data) {
       this.topicAddedResult = "Topic created at: " + data.data.insert_kgraph_topics.returning[0].created_at
@@ -139,9 +178,31 @@ export default {
       console.log("---- getTopics() ---");
       console.log(data);
       let topicsTree = [];
-      buildTopicsTree(topicsTree, data.kgraph_topics)
+      let topicsRelations = [];
+      buildTopicsTree(topicsTree, topicsRelations, data.kgraph_topics)
       this.topics = topicsTree;
-    }
+      this.topicsRelations = topicsRelations;
+    },
+    buildVisGraph() {
+      console.log("buildVisGraph")
+      if (this.network) {
+          this.network.destroy();
+          this.network = null;
+          // this.nodes.clear();
+          // this.edges.clear();
+      } 
+      this.nodes = new DataSet(this.topics)
+      this.edges = new DataSet(this.topicsRelations);
+      let data = {
+        nodes: this.nodes,
+        edges: this.edges
+      };
+      const element = document.getElementById('vis-topics-graph');
+      this.network = new Network(element, data, this.displayOptions);
+      // this.network.enableEditMode();
+      // this.network.stopSimulation();
+      // network.on('selectNode', event => this.onSelectNode(network, event));
+    },
   },
 }
 </script>
@@ -205,5 +266,14 @@ li {
   box-sizing: border-box;
   overflow: visible;
   padding: 50px;
+  /* to center alignment everything */
+  display: flex;
+  justify-content: center;
+}
+
+#vis-topics-graph {
+  width: 1024px;
+  height: 400px;
+  border: 1px solid lightgray;
 }
 </style>

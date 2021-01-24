857922e564487db67a8eff543a894e5d934b8a54
Gian Luigi Romita <romitagl@gmail.com>
2021-01-24 17:33:25 +0000

    Fixed linting errors

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createProvider } from './vue-apollo'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 

8027c53af47fa97f026f43810c467cfdae5cf771
Gian Luigi Romita <romitagl@gmail.com>
2021-01-31 14:07:07 +0000

    Add topic on context - CI update (#8)
    * Fix CI
    
    * Added Vuetify library
    
    * Fixed linting errors
    
    * Fixed linting errors
    
    * Fixed linting errors
    
    * Fixed linting errors
    
    * Fix typo
    
    * Added Node Insert & Updated Hasura tests
    
    * Trigger CI workflow on comment or after linting

--- backend/hasura/test/test-graphql.sh
+++ backend/hasura/test/test-graphql.sh
@@ -2,20 +2,20 @@
 
 if [[ ! $HASURA_CURL_POST_COMMAND || ! "$HASURA_GRAPHQL_ENDPOINT" ]]; then printf "missing required test env variables\n" && exit 1; fi
 
-USERNAME='ci_test'
-TOPICNAME='test topic'
+kg_test_username=${KG_TEST_USERNAME:-"ci_test"}
+kg_test_topicname=${TOPICNAME:-"test topic"}
 
 function test_add_topic () {
   # 0. cleanup the users table
-  $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { delete_kgraph_users(where: {username: {_eq: \\\"$USERNAME\\\"}}) { affected_rows }}\" }" "$HASURA_GRAPHQL_ENDPOINT"
+  $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { delete_kgraph_users(where: {username: {_eq: \\\"$kg_test_username\\\"}}) { affected_rows }}\" }" "$HASURA_GRAPHQL_ENDPOINT"
 
   # 1. create an entry in the users table
-  INSERT_RET_VAL=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_users(objects: {username: \\\"$USERNAME\\\", display_name: \\\"$USERNAME\\\"}) { returning { created_at } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
-  printf "created user: %s at: %s\n" "$USERNAME" "$INSERT_RET_VAL"
+  INSERT_RET_VAL=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_users(objects: {username: \\\"$kg_test_username\\\", display_name: \\\"$kg_test_username\\\"}) { returning { created_at } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
+  printf "created user: %s at: %s\n" "$kg_test_username" "$INSERT_RET_VAL"
 
   # 2. add a topic
-  TOPIC_ID_RESULT=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_topics(objects: {users_username: \\\"$USERNAME\\\", name: \\\"$TOPICNAME\\\"}) { returning { id } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
-  printf "added topic: %s with id: %s\n" "$TOPICNAME" "$TOPIC_ID_RESULT"
+  TOPIC_ID_RESULT=$( $HASURA_CURL_POST_COMMAND "{ \"query\": \"mutation { insert_kgraph_topics(objects: {users_username: \\\"$kg_test_username\\\", name: \\\"$kg_test_topicname\\\"}) { returning { id } } }\" }" "$HASURA_GRAPHQL_ENDPOINT" )
+  printf "added topic: %s with id: %s\n" "$kg_test_topicname" "$TOPIC_ID_RESULT"
 
   # 3. get the topic by ID
   TOPIC_ID=$( echo "$TOPIC_ID_RESULT" | jq .data.insert_kgraph_topics.returning[0].id | tr -d '"' )
@@ -24,7 +24,7 @@ function test_add_topic () {
 
   TOPICNAME_NAME=$( echo "$TOPICNAME_RESULT" | jq .data.kgraph_topics[0].name | tr -d '"' )
   # 4. verify that TOPICNAME matches TOPICNAME_NAME
-  if [ "$TOPICNAME" != "$TOPICNAME_NAME" ]; then
+  if [[ "$kg_test_topicname" != "$TOPICNAME_NAME" ]]; then
     exit 1;
   fi
 }
@@ -36,8 +36,8 @@ function test_drop_all_data () {
 
 function test_check_ci_topic () {
 
-  TOPICID_RESULT=$( $HASURA_CURL_POST_COMMAND "{\"query\": \"query { kgraph_topics(where: {users_username: {_eq: \\\"$USERNAME\\\" }, name: {_eq: \\\"$TOPICNAME\\\"}}) { id }}\" }" "$HASURA_GRAPHQL_ENDPOINT" )
-  printf "get topic: %s with name: %s\n" "$TOPICID_RESULT" "$TOPICNAME"
+  TOPICID_RESULT=$( $HASURA_CURL_POST_COMMAND "{\"query\": \"query { kgraph_topics(where: {users_username: {_eq: \\\"$kg_test_username\\\" }, name: {_eq: \\\"$kg_test_topicname\\\"}}) { id }}\" }" "$HASURA_GRAPHQL_ENDPOINT" )
+  printf "get topic: %s with name: %s\n" "$TOPICID_RESULT" "$kg_test_topicname"
   TOPICID=$( echo "$TOPICID_RESULT" | jq .data.kgraph_topics[0].id | tr -d '"' )
 
   if [[ ! "$TOPICID" ]]; then

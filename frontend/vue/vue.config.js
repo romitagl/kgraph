855df87aab24ad58dc749bc4ba9b6fb13e863b58
Gian Luigi Romita <romitagl@gmail.com>
2021-07-04 12:19:39 +0100

    Updated GitHub Actions

--- frontend/vue/vue.config.js
+++ frontend/vue/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'demo'
-  // ? '/kgraph/'
-  // : '/',
+  publicPath: process.env.NODE_ENV === 'demo'
+  ? '/kgraph/'
+  : '/',
   devServer: {
     port: 5000
   },
